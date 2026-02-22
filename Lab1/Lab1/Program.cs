160f03c85975feacb6de26c28e9306f292d31746
octomors <stepanka4444@gmail.com>
2026-02-22 15:06:59 +0700

    lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+[STAThread]
+static void Main()
+{
+    using var dialog = new OpenFileDialog { Filter = "Images|*.jpg;*.png" };
+    if (dialog.ShowDialog() == DialogResult.OK)
+    {
+        ProcessImage(dialog.FileName);
+    }
+}
